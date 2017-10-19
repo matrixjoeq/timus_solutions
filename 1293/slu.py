ea37d53434631092afa1ce847ebabe4f4686364b
Qu Xing <xqu@telenav.com>
2017-10-19 09:49:47 +0800

    Accepted 1292 (0.031	348 KB).

--- 1293/slu.py
+++ 1293/slu.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 '''
 1293. Eniya
@@ -9,17 +9,17 @@ Memory limit: 64 MB
 [Description]
 It’s restless now on the slips of the intergalactic port’s sixth dock of planet of Torn.
 No longer then in a month the reconstruction of the small ironclad corvette “Eniya” will
-be finished. And again this battle ship and its brave team would have to struggle for 
-the control over plutonium mines of Sibelius. 
-The work cannot be stopped even for a second, self-powered laser welders work round the 
+be finished. And again this battle ship and its brave team would have to struggle for
+the control over plutonium mines of Sibelius.
+The work cannot be stopped even for a second, self-powered laser welders work round the
 clock. Joints of robots-repairers fuse because of this permanent work. Nevertheless, they
 can’t stop not for a single moment.
 
-Now in all this turmoil it is discovered that corvette’s thermopanels again need an 
+Now in all this turmoil it is discovered that corvette’s thermopanels again need an
 urgent processing with thorium sulphide. It is known that the processing of the one square
-meter of the panel needs 1 nanogramm of sulphide. In general, it is needed to process N 
+meter of the panel needs 1 nanogramm of sulphide. In general, it is needed to process N
 rectangular panels, which dimensions are A by B meters. It is necessary to calculate as
-fast as possible, how much sulphide is needed in general for the processing of all panels 
+fast as possible, how much sulphide is needed in general for the processing of all panels
 of “Eniya”. Moreover, do not forget, that the panels need processing of both sides.
 
 [Input]
@@ -32,6 +32,15 @@ Output the weight of thorium sulphide in nanogramms needed for the processing.
 import sys;
 import math;
 
+def calc():
+    line = sys.stdin.readline()
+    args = line.split(' ')
+    n = int(args[0])
+    a = int(args[1])
+    b = int(args[2])
+    weight = n * a * b * 2
+    print weight
+
 if __name__ == '__main__':
-    pass
-    
+    calc()
+
