161c809474b75d01453e403a4a0f95bb364231ab
Qu Xing <xqu@telenav.com>
2017-10-31 14:22:03 +0800

    Accepted 1496 (0.031	360 KB)

---
+++ 1496/slu.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+1496. Spammer
+Time limit: 1.0 second
+Memory limit: 64 MB
+
+[Description]
+There is a famous spammer among our friends. In the end of every contest he is
+submitting his incorrect solutions at the speed of a machine-gun. In addition,
+he cheats by means of using several accounts for debugging. The judges eventually
+decided to disqualify the spammer. For that, they first want to discover all of
+his debugging accounts. The judges know which teams submitted their solutions
+in the last ten minutes of a contest. They regard as the spammer's debugging
+accounts all accounts which submitted solutions at least twice during the last
+ten minutes of the contest. Your task is to find these accounts.
+
+[Input]
+The first line contains the number N of submissions in the last 10 minutes
+(0 ≤ N ≤ 100). The next N lines contain the names of teams which submitted
+their solutions in this period. The names contain only digits and lowercase
+English letters. Each name is not longer than 30 symbols.
+
+[Output]
+Output in an arbitrary order all accounts which, in the judges' opinion, are
+used by the spammer.
+'''
+
+import sys;
+import math;
+
+def calc():
+    n = int(sys.stdin.readline())
+    teams = {}
+    while (n > 0):
+        n = n - 1
+        name = sys.stdin.readline().strip('\r\n')
+        if (teams.has_key(name)):
+            teams[name] = teams[name] + 1
+        else:
+            teams[name] = 1
+
+    for k, v in teams.items():
+        if (v >= 2):
+            print k
+
+if __name__ == '__main__':
+    calc()
+
