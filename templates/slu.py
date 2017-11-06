1abd448d047f88f77b9302570f9eaa4ef1fa9619
Qu Xing <xqu@telenav.com>
2017-11-06 09:58:47 +0800

    update python template

--- templates/slu.py
+++ templates/slu.py
@@ -10,18 +10,21 @@
 
 '''
 
-import sys;
-import math;
+import sys
+import math
+
 
 def get_str_from_stdin():
     return sys.stdin.readline().strip('\r\n')
 
+
 def get_int_from_stdin():
     return int(get_str_from_stdin())
+
 
 def calc():
     pass
 
+
 if __name__ == '__main__':
     calc()
-
