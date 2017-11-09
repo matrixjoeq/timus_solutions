9ff135b1af97448eaafd5852b87e015d8439c3cb
Qu Xing <xqu@telenav.com>
2017-11-09 16:16:03 +0800

    Accepted 1044 (0.031	508 KB)

--- 1044/slu.py
+++ 1044/slu.py
@@ -35,13 +35,34 @@ should contain a number of tickets such that the sum of the first half of
 digits is equal to the sum of the second half of digits.
 '''
 
-import sys;
-import math;
+import sys
+import math
+
 
 def calc():
     n = int(sys.stdin.readline())
+    n = n / 2
+    max_n = int(math.pow(10, n))
+    sum_d = {}
+    digits = []
+    for i in range(n):
+        digits.append(0)
+
+    for i in range(max_n):
+        for j in range(n):
+            digits[j] = i % 10
+            i = i / 10
+        s = sum(digits)
+        if s in sum_d.keys():
+            sum_d[s] = sum_d[s] + 1
+        else:
+            sum_d[s] = 1
+    r = 0
+    for v in sum_d.values():
+        r = r + v * v
+
+    print r
 
 
 if __name__ == '__main__':
     calc()
-
