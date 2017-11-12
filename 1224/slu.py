d50984664046516d954939f9d2541b8147f946b5
matrixjoeq <matrix0932joeq@163.com>
2017-11-12 23:13:15 +0800

    Accepted 1224 (0.046	372 KB)

--- 1224/slu.py
+++ 1224/slu.py
@@ -22,21 +22,55 @@ The input contains two integers in the following order: N, M (1 ≤ N, M ≤ 2^3
 The output consists of a single integer value — the number of the turns.
 '''
 
-import sys;
-import math;
+import sys
+import math
 
 def get_str_from_stdin():
     return sys.stdin.readline().strip('\r\n')
 
+
 def get_int_from_stdin():
     return int(get_str_from_stdin())
+
+
+def t(n, m):
+    if n == 1:
+        return 0
+
+    if m == 1:
+        # here n must be larger than 1
+        return 1
+
+    if n == 2:
+        # here m must be larger than 1
+        return 2
+
+    if m == 2:
+        # here n must be larger than 2
+        return 3
+
+    if m >= n:
+        if n % 2 == 0:
+            p = n / 2 - 1
+            return t(2, m - 2 * p) + 4 * p
+        else:
+            p = (n - 1) / 2
+            return t(1, m - 2 * p) + 4 * p
+    else:
+        if m % 2 == 0:
+            p = m / 2 - 1
+            return t(n - 2 * p, 2) + 4 * p
+        else:
+            p = (m - 1) / 2
+            return t(n - 2 * p, 1) + 4 * p
+
 
 def calc():
     n, m = get_str_from_stdin().split(' ')
     n = int(n)
     m = int(m)
+    print t(n, m)
 
 
 if __name__ == '__main__':
     calc()
-
